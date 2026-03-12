7adfa6dc242e94fbea6fcab4f702e76c4cfbe79e
Gab02022 <gabrielmmarquez@gmail.com>
2026-03-12 05:59:29 +0000

    Add ueransim

--- 03-argocd/main.tf
+++ 03-argocd/main.tf
@@ -1,37 +1,69 @@
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  namespace        = "argocd"
-  create_namespace = true
-  version          = "5.51.6" # Usamos una versión estable
+# 2. Inyectar Apps (Core 5G y UERANSIM) automáticamente
+resource "helm_release" "argocd_apps" {
+  name       = "argocd-apps"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  namespace  = "argocd"
+  version    = "1.6.2" 
+  
+  depends_on = [helm_release.argocd]
 
-  # Leer repo de K8s automáticamente
   values = [
     <<-EOT
-    server:
-      # Desactivamos TLS local
-      extraArgs:
-        - --insecure
-      
-      # ArgoCD qué repositorio debe vigilar
-      additionalApplications:
-        - name: open5gs-lab
-          namespace: argocd
-          project: default
-          source:
-            repoURL: https://github.com/Gab02022/open5gs-k8s-gcp-test.git
-            targetRevision: HEAD
-            path: charts/open5gs 
-          destination:
-            server: https://kubernetes.default.svc
-            namespace: open5gs 
-          syncPolicy:
-            automated:
-              prune: true
-              selfHeal: true
-            syncOptions:
-              - CreateNamespace=true 
+    applications:
+      # 1. El Core 5G (Open5GS)
+      - name: open5gs
+        namespace: argocd
+        project: default
+        source:
+          repoURL: 'https://github.com/Gab02022/open5gs-k8s-gcp-test.git'
+          targetRevision: HEAD
+          path: charts/open5gs
+        destination:
+          server: 'https://kubernetes.default.svc'
+          namespace: open5gs
+        syncPolicy:
+          automated:
+            prune: true
+            selfHeal: true
+          syncOptions:
+            - CreateNamespace=true
+
+      # 2. UERANSIM - (gNodeB)
+      - name: ueransim-gnb
+        namespace: argocd
+        project: default
+        source:
+          repoURL: 'https://github.com/Gab02022/open5gs-k8s-gcp-test.git'
+          targetRevision: HEAD
+          path: charts/ueransim-gnb 
+        destination:
+          server: 'https://kubernetes.default.svc'
+          namespace: ueransim     
+        syncPolicy:
+          automated:
+            prune: true
+            selfHeal: true
+          syncOptions:
+            - CreateNamespace=true
+
+      # 3. UERANSIM - (UEs)
+      - name: ueransim-ues
+        namespace: argocd
+        project: default
+        source:
+          repoURL: 'https://github.com/Gab02022/open5gs-k8s-gcp-test.git'
+          targetRevision: HEAD
+          path: charts/ueransim-ues 
+        destination:
+          server: 'https://kubernetes.default.svc'
+          namespace: ueransim  
+        syncPolicy:
+          automated:
+            prune: true
+            selfHeal: true
+          syncOptions:
+            - CreateNamespace=true
     EOT
   ]
 }
